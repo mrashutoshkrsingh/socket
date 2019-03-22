92f99e5122b434d836a14ba74c7b07b49ff44bff
mrashutoshkrsingh <mrashutoshkrsingh@gmail.com>
2019-03-22 11:14:30 +0530

    first commit

---
+++ socket_1/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const socketio = require("socket.io");
+
+app.use(express.static(__dirname + "/public"));
+
+const expressServer = app.listen(9000);
+
+const io = socketio(expressServer);
+
+io.on("connection", socket => {
+  socket.emit("messageFromServer", { data: "This is the message from server" });
+
+  socket.on("dataToServer", dataFromClient => {
+    console.log(dataFromClient);
+  });
+});
